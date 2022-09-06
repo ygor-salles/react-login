a3e32513f910eb6316f6a625b3d65544a5fb098e
ygor-salles <ygorsallesaniceto@gmail.com>
2022-09-05 22:55:56 -0300

    interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,19 @@
 import axios from "axios";
+import { getUserLocalStorage } from "../AuthProvider/util";
 
 export const Api = axios.create({
   baseURL: "https://reqres.in/api/",
 });
+
+Api.interceptors.request.use(
+  (config) => {
+    const user = getUserLocalStorage();
+
+    if (config.headers) config.headers.Authorization = user?.token;
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
